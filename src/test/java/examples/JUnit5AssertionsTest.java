5bc543b7311a71523a4ed2b1753e7a0b30cdca56
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-15 09:14:49 +0100

    finished test for isPrime method

--- src/test/java/examples/JUnit5AssertionsTest.java
+++ src/test/java/examples/JUnit5AssertionsTest.java
@@ -1,8 +1,63 @@
 package examples;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class JUnit5AssertionsTest {
 
     private JUnit5Assertions test = new JUnit5Assertions();
 
     @Test
+    public void shouldBePrimeFor1(){
+        //when
+        boolean result = test.isPrimeNumber(1);
+
+        //then
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void shouldBePrimeFor2(){
+        //when
+        boolean result = test.isPrimeNumber(2);
+
+        //then
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void shouldNotBePrimeFor10(){
+        //when
+        boolean result = test.isPrimeNumber(10);
+
+        //then
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void shouldNotBePrimeFor11(){
+        //when
+        boolean result = test.isPrimeNumber(11);
+
+        //then
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void shouldNotBePrimeFor13(){
+        //when
+        boolean result = test.isPrimeNumber(13);
+
+        //then
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void shouldNotBePrimeFor9(){
+        //when
+        boolean result = test.isPrimeNumber(9);
+
+        //then
+        Assertions.assertTrue(result);
+    }
 }
