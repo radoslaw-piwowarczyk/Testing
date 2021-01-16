69537c7c1a9bc38010e9e5786a6caae6697a1bf5
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-16 10:04:05 +0100

    finished first CsvSource test

--- src/test/java/AssertJ/MonthCalculatorParametrizedTest.java
+++ src/test/java/AssertJ/MonthCalculatorParametrizedTest.java
@@ -3,6 +3,7 @@ package AssertJ;
 import JUnit5.MonthCalculator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.YearMonth;
@@ -12,15 +13,27 @@ public class MonthCalculatorParametrizedTest {
     private MonthCalculator monthCalculator = new MonthCalculator();
 
     @ParameterizedTest
-    @ValueSource(strings = {"2001-01","2001-03","2001-05","2001-07","2001-08","2001-10","2001-12"})
-    public void shouldReturn31Days(String month){
+    @ValueSource(strings = {"2001-01", "2001-03", "2001-05", "2001-07", "2001-08", "2001-10", "2001-12"})
+    public void shouldReturn31Days(String month) {
         //given
         YearMonth yearMonth = YearMonth.parse(month);
 
         //when
-        int dayCount = monthCalculator.getDayCount(yearMonth.getYear(),yearMonth.getMonthValue());
+        int dayCount = monthCalculator.getDayCount(yearMonth.getYear(), yearMonth.getMonthValue());
 
         //then
         Assertions.assertThat(dayCount).isEqualTo(31);
     }
+
+    @ParameterizedTest
+    @CsvSource({"2001,04", "2001,06", "2001,09", "2001,11"})
+    public void shouldReturn30Days(int year, int month) {
+        //when
+        int dayCount = monthCalculator.getDayCount(year, month);
+
+        //then
+        Assertions.assertThat(dayCount).isEqualTo(30);
+    }
+
+
 }
