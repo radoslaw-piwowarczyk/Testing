fd8a4fe67dff92433c56fdc68e3f7352ba87d618
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-24 20:03:46 +0100

    added mock addnotations

--- src/test/java/Mockito/SmartHomeControllerTest.java
+++ src/test/java/Mockito/SmartHomeControllerTest.java
@@ -1,17 +1,31 @@
 package Mockito;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 
 public class SmartHomeControllerTest {
 
+    @Mock
+    private HumiditySensor humiditySensorMock;
+    @Mock
+    private WateringController wateringControllerMock;
+
+    @BeforeEach
+    void init(){
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     void shouldDisableWatering() {
-        HumiditySensor humiditySensorMock = Mockito.mock(HumiditySensor.class);
-        Mockito.when(humiditySensorMock.getHumidity()).thenReturn(50);
+//        HumiditySensor humiditySensorMock = Mockito.mock(HumiditySensor.class);
+//        WateringController wateringControllerMock = Mockito.mock(WateringController.class);
 
-        WateringController wateringControllerMock = Mockito.mock(WateringController.class);
+        //given
+        Mockito.when(humiditySensorMock.getHumidity()).thenReturn(50);
         SmartHomeController smartHomeController = new SmartHomeController(humiditySensorMock, wateringControllerMock);
 
         //when
