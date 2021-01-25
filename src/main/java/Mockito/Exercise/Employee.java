85c445547ec69f0d76d7aecb1225ee89dc41d5ec
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-25 08:15:59 +0100

    added implementation to Employee class

--- src/main/java/Mockito/Exercise/Employee.java
+++ src/main/java/Mockito/Exercise/Employee.java
@@ -1,4 +1,22 @@
 package Mockito.Exercise;
 
+import java.math.BigDecimal;
+
 public class Employee {
+
+    private String name;
+    private BigDecimal baseSalary;
+
+    public Employee(String name, BigDecimal baseSalary) {
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getBaseSalary() {
+        return baseSalary;
+    }
 }
