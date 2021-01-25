0191899a9026872a3df78f2432b89eedda7e4b09
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-25 08:17:01 +0100

    added implementation to TaskManagementSystem  class

--- src/main/java/Mockito/Exercise/TaskManagementSystem.java
+++ src/main/java/Mockito/Exercise/TaskManagementSystem.java
@@ -1,4 +1,10 @@
 package Mockito.Exercise;
 
+import java.util.Random;
+
 public class TaskManagementSystem {
+
+    public int countFinishedTasksForEmployee(Employee employee){
+        return new Random().nextInt(30);
+    }
 }
