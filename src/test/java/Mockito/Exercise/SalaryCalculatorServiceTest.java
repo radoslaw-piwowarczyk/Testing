7c0d357905a2368f1fcd972f4f3343fa22277c79
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-25 08:36:01 +0100

    finished shouldHavenoBonusTest

--- src/test/java/Mockito/Exercise/SalaryCalculatorServiceTest.java
+++ src/test/java/Mockito/Exercise/SalaryCalculatorServiceTest.java
@@ -1,8 +1,14 @@
 package Mockito.Exercise;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+
 
 public class SalaryCalculatorServiceTest {
 
@@ -12,8 +18,21 @@ public class SalaryCalculatorServiceTest {
     private SalaryCalculatorService salaryCalculatorService;
 
     @BeforeEach
-    public void init(){
+    public void init() {
         MockitoAnnotations.openMocks(this);
         salaryCalculatorService = new SalaryCalculatorService(taskManagementSystem);
+    }
+
+    @Test
+    public void shouldHaveNoBonus() {
+        //given
+        Employee employee = new Employee("Tomek", BigDecimal.valueOf(2000));
+        Mockito.when(taskManagementSystem.countFinishedTasksForEmployee(employee)).thenReturn(9);
+
+        //when
+        BigDecimal salary = salaryCalculatorService.calculateSalary(employee);
+
+        //then
+        Assertions.assertThat(salary).isEqualTo(BigDecimal.valueOf(2000));
     }
 }
